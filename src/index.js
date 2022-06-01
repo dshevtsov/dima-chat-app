888a69bda15d9b1c7a69096475ba5f39776c338c
Dmytro Shevtsov <shevtsov@adobe.com>
2022-05-31 20:35:07 -0500

    Init commit

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { getUser, getUsersInRoom, addUser, removeUser } = require('./utils/users')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectory = path.join(__dirname, '../public')
+
+// Set up static directory to serve
+app.use(express.static(publicDirectory))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({ id: socket.id, ...options })
+
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage('Admin','Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin' ,`${user.username} has joined!`))    
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
+
+    socket.on('sendMessage', (inputText, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(inputText)) {
+            return callback('Profanity is not allowed')
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, inputText))
+        callback()
+    })
+
+    socket.on('sendLocation', (position, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${position.latitude},${position.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is app on port ${port}!`)
+})
